990dfa9d0872c1eba130e9acbb928efd662f4e40
guacho20 <jhon_199713@hotmail.com>
2021-06-07 16:42:21 -0500

    modificaciones dashboard

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService, UtilitarioService } from 'ngprime-core';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  fechaUltimoAcceso: string;
+  pantallas: [];
+  userAgent: any = null;
+  usuario = [];
+  sistemaOperativo: any = null;
+  dispositivo: string;
+
+  constructor(
+    private authSvc: AuthService,
+    private utilitarioSvc: UtilitarioService
+  ) {
+    this.userAgent = this.utilitarioSvc.getUserAgent();
+    this.usuario = authSvc.getUserData();
+    this.dispositivo = this.utilitarioSvc.getPlataforma();
+    this.sistemaOperativo = this.utilitarioSvc.getSistemaOperativo();
+  }
 
   ngOnInit(): void {
+    this.fechaUltimoAcceso = localStorage.getItem('ultimoAcceso');
+    this.authSvc.pantallasMasUsadas().subscribe(resp => {
+      this.pantallas = resp.datos;
+    });
   }
 
 }
